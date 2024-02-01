8aa911780da82d7ee69469931829eb71c8ed87cb
DSatSPS <soucekd23@spstrutnov.cz>
2024-02-01 13:20:45 +0100

    feat: added post-processing

--- src/fd_render.py
+++ src/fd_render.py
@@ -4,13 +4,32 @@ import OpenGL.GL as gl
 # == Far Depths renderer ==
 # a OpenGL / PyGame hybrid abomination
 
+def load_shader(shader_type, path):
+    src = open(path, "r")
+
+    s = gl.glCreateShader(shader_type)
+
+    gl.glShaderSource(s, src.read())
+    gl.glCompileShader(s)
+
+    result = gl.glGetShaderiv(s, gl.GL_COMPILE_STATUS)
+
+    if not result:
+        log = gl.glGetShaderInfoLog(s)
+
+        print("error while compiling a Gl shader: ", log)
+        quit()
+
+    return s
+
 class FDContext:
     def __init__(self, res):
         self.win = pg.display.set_mode(res, pg.OPENGL | pg.DOUBLEBUF, vsync=1)
         
         self.create_pg_framebuffer(res)
+        self.set_post_process_shader("src/uber_post.vert", "src/uber_post.frag")
 
-    # creates a opengl rgba texture that can store the pygame surface
+    # creates a "shared" pygame opengl texture framebuffer
     def create_pg_framebuffer(self, res):
         self.pg_fb = pg.Surface(res)
         self.pg_fb_texture = gl.glGenTextures(1)
@@ -37,6 +56,42 @@ class FDContext:
 
         gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
 
+    # load post-processing program
+    def set_post_process_shader(self, vert, frag):
+        vs = load_shader(gl.GL_VERTEX_SHADER, vert)
+        fs = load_shader(gl.GL_FRAGMENT_SHADER, frag)
+
+        p = gl.glCreateProgram()
+
+        gl.glAttachShader(p, vs)
+        gl.glAttachShader(p, fs)
+    
+        gl.glLinkProgram(p)
+
+        result = gl.glGetProgramiv(p, gl.GL_LINK_STATUS)
+
+        if not result:
+            log = gl.glGetProgramInfoLog(p)
+
+            print("error while linking a Gl shader program: ", log)
+            quit()
+
+        gl.glDeleteShader(vs)
+        gl.glDeleteShader(fs)
+
+        self.post_process_pass = p
+
+    def post_process(self):
+        # gl.glClearColor(.1, .1, .1, 1)
+        # gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+        gl.glUseProgram(self.post_process_pass)
+
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.pg_fb_texture)
+        # gl.glUniform1f(time_loc, time_)
+    
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3)
+
 # == init the renderer ==
 
 fd_renderer = FDContext((1280, 720))
@@ -51,6 +106,10 @@ def get_surface():
 def submit():
     fd_renderer.flip_pg_framebuffer()
 
-    # post-process
+    # post-processing pass
+
+    fd_renderer.post_process()
+
+    # frame finished
 
     pg.display.flip() # flips the window (opengl) surface
